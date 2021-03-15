669ac156110fba433e5dd1fd118b2f42c25182b2
심승용 <seungyongshim@nexon.co.kr>
2021-03-15 11:14:08 +0900

    수정

--- tests/sample/kafka_simple_tests.py
+++ tests/sample/kafka_simple_tests.py
@@ -6,14 +6,18 @@ def test_simple_publish():
     topic = "com.nexon.ibms.message.test1"
     consumer_group_id = "test1"
     consumer = KafkaConsumer(
-        topic, bootstrap_servers="localhost:9092", group_id=consumer_group_id
+        topic,
+        bootstrap_servers="localhost:9092",
+        group_id=consumer_group_id,
+        value_deserializer=lambda m: json.loads(m.decode("utf-8")),
     )
     producer = KafkaProducer(
         bootstrap_servers="localhost:9092",
         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
     )
+
     producer.send(topic, {"test": "something"}).get(timeout=10)
 
-    msgs = next(consumer)
+    msg = next(consumer)
 
-    assert "hello" in msgs
+    assert {"test": "something"} == msg.value
